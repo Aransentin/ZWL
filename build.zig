f3ab756d07178a83dce0792f5fc50087ad4e1d83
Aransentin <jens.goldberg@gmail.com>
2020-10-18 21:21:48 +0000

    Corrected build steps

--- build.zig
+++ build.zig
@@ -11,17 +11,15 @@ pub fn build(b: *Builder) void {
     tests.setBuildMode(mode);
     tests_step.dependOn(&tests.step);
 
-    const example_build_step = b.step("example", "Build the example");
     const example_exe = b.addExecutable("example", "example.zig");
     example_exe.addPackagePath("zwl", "src/zwl.zig");
     example_exe.single_threaded = true;
     example_exe.setTarget(target);
     example_exe.setBuildMode(mode);
-    example_exe.linkLibC();
+    // example_exe.linkLibC();
+    example_exe.install();
 
-    example_exe.setOutputDir("zig-cache/bin");
-    example_build_step.dependOn(&example_exe.step);
-
+    const run_cmd = example_exe.run();
     const example_run_step = b.step("run", "Run the example");
-    example_run_step.dependOn(&example_exe.run().step);
+    example_run_step.dependOn(&run_cmd.step);
 }
