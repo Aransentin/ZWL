77e5c8f01e567e09dd05d3451f317ec038d321cc
Christian Wesselhoeft <hi@xtian.us>
2020-12-09 14:38:53 -0700

    gdi32.zig: SizeOf -> sizeOf

--- src/windows/gdi32.zig
+++ src/windows/gdi32.zig
@@ -29,7 +29,7 @@ pub const DIBColors = extern enum {
 };
 
 pub const BITMAPINFOHEADER = extern struct {
-    biSize: DWORD = @SizeOf(@This()),
+    biSize: DWORD = @sizeOf(@This()),
     biWidth: LONG,
     biHeight: LONG,
     biPlanes: WORD,
